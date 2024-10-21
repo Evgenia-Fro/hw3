1da96edc2938148172aefb8c69f70d61648cb45f
Евгения <ilyasova493@gmail.com>
2024-10-21 15:12:02 +0300

    21.10.24

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+my_string=input('Введите текст: ')
+print(my_string)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+bykva = my_string[0]
+print(bykva)
+symbol=my_string[-1]
+print(symbol)
+
